08b77b1971e225cc01bbbf0c2d6d0780476849c4
karisha06 <roverova1@icloud.com>
2024-02-05 13:51:05 +0300

    Выполнили работу с объектами

--- task3/main.js
+++ task3/main.js
@@ -1,22 +1,21 @@
-// let students = ["Андрей", "Иван", "Стас"];
-// students[50] = "Индекс 50"
-// console.log(students);
+// let person = {
+//     name: "Tom",
+//     age: 18,
+//     hobbies: ["Кушать", "Спать", "Просмотр кино"],
+//     contacts: {
+//         phone: [89938830249, 4893809549],
+//         email: ["tom@gmail.com", "tom2@icloud.com"],
+//         social: "www.github.com"
+//     }
+// }
+// console.log(person["name"], person["age"]);
+// console.log(person["hobbies"][2]);
+// console.log(person["contacts"]["email"][1]);
 
-// let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
-// college[1][1] = 112
-// let kab = college[1][1]
-// console.log(kab)
-
-let number = [ 1,2,3,4,5,6,7 ];
-
-const coutLength = 5;
-if ( number.length === 5 ) {
-    console.log("ok")
-} else if ( number.length < coutLength) {
-    let result = coutLength - number.length;
-    console.log(`Длина массива не равна: ${number.length}. Длину массива нужно увеличить до ${coutLength} Прибавьте к массиву значение ${result}`)
-} else if ( number.length > coutLength ) {
-    result = number.length - coutLength;
-    console.log(`Длина массива не равна: ${number.length}. Длину массива нужно уменьшить до ${coutLength} Удалите из массива значение ${result}` )
-}
-    
+let college = [
+    {name: "Tom", age: 23, group: "ИС-2-2"}, 
+    {name: "Bob", age: 15, group: "ИС-2-1"}, 
+    {name: "Stan", age: 23, group: "ИС-2-2"}, 
+    {name: "Harry", age: 17, group: "ИС-4-1"} 
+];
+console.log(college[2]["name"]);
